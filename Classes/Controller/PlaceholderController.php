fcba42d46ca35209bd18be226336991b727f2250
Christian <indy.essl@gmail.com>
2019-02-13 18:34:08 +0100

    [REFACTOR] refactor PlaceholderController

--- Classes/Controller/PlaceholderController.php
+++ Classes/Controller/PlaceholderController.php
@@ -13,12 +13,15 @@ namespace ChristianEssl\PlaceholderImages\Controller;
  ***/
 
 use ChristianEssl\PlaceholderImages\Exception\InvalidConfigurationException;
+use ChristianEssl\PlaceholderImages\Exception\MissingArgumentsException;
+use ChristianEssl\PlaceholderImages\Resource\Placeholder\AbstractProcessor;
 use ChristianEssl\PlaceholderImages\Resource\Placeholder\CustomSourceProcessor;
 use ChristianEssl\PlaceholderImages\Resource\Placeholder\LocalImageProcessor;
 use ChristianEssl\PlaceholderImages\Resource\Placeholder\PlaceholderComProcessor;
 use ChristianEssl\PlaceholderImages\Utility\ConfigurationUtility;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Resource\Exception\FileDoesNotExistException;
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -38,12 +41,44 @@ class PlaceholderController
     public function createAction(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $targetFolderIdentifier = $request->getParsedBody()['targetFolder'];
+        $imageSettings = $this->getImageSettings($request);
+        $data = [];
+
+        try {
+            $file = $this->getFile($imageSettings, $targetFolderIdentifier);
+            $data['file'] = $file->getUid();
+        } catch (InvalidConfigurationException $e) {
+            $data['error'] = $this->getTranslation('tx_placeholderimages.image.error.invalid_configuration');
+        } catch (FileDoesNotExistException $e) {
+            $data['error'] = $this->getTranslation('tx_placeholderimages.image.error.unknown_error');
+        } catch (MissingArgumentsException $e) {
+            $data['error'] = $this->getTranslation('tx_placeholderimages.image.error.empty_request');
+        }
+
+        return $this->getJsonResponse($response, $data);
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @param array $data
+     *
+     * @return ResponseInterface
+     */
+    protected function getJsonResponse(ResponseInterface $response, array $data) : ResponseInterface
+    {
+        $response->getBody()->write(json_encode($data));
+        return $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     *
+     * @return string[]
+     */
+    protected function getImageSettings(ServerRequestInterface $request) : array
+    {
         $width = (int) $request->getParsedBody()['width'];
         $height = (int) $request->getParsedBody()['height'];
-        $format = $request->getParsedBody()['format'];
-        $placeholder = $request->getParsedBody()['placeholder'];
-        $bgcolor = $request->getParsedBody()['bgcolor'];
-        $textcolor = $request->getParsedBody()['textcolor'];
 
         if (!$width) {
             $width = $height;
@@ -52,44 +87,46 @@ class PlaceholderController
             $height = $width;
         }
 
-        $imageSettings = [
+        return [
             'width' => $width,
             'height' => $height,
-            'format' => $format,
-            'placeholder' => $placeholder,
-            'bgcolor' => $bgcolor,
-            'textcolor' => $textcolor,
+            'format' => $request->getParsedBody()['format'],
+            'placeholder' => $request->getParsedBody()['placeholder'],
+            'bgcolor' => $request->getParsedBody()['bgcolor'],
+            'textcolor' => $request->getParsedBody()['textcolor'],
         ];
-
-        if (!empty($width)) {
-            $data = [];
-            try {
-                $file = $this->getFile($imageSettings, $targetFolderIdentifier);
-                if ($file !== null) {
-                    $data['file'] = $file->getUid();
-                } else {
-                    $data['error'] = $this->getTranslation('tx_placeholderimages.image.error.unknown_error');
-                }
-            } catch (InvalidConfigurationException $e) {
-                $data['error'] = $this->getTranslation('tx_placeholderimages.image.error.invalid_configuration');
-            }
-            $response->getBody()->write(json_encode($data));
-            return $response;
-        }
-        $response->getBody()->write(json_encode([
-            'error' => $this->getTranslation('tx_placeholderimages.image.error.empty_request')
-        ]));
-        return $response;
     }
 
     /**
      * @param array $imageSettings
      * @param string $targetFolderIdentifier
      *
-     * @throws InvalidConfigurationException
      * @return File|null
+     * @throws MissingArgumentsException
+     * @throws InvalidConfigurationException
+     * @throws FileDoesNotExistException
      */
     protected function getFile($imageSettings, $targetFolderIdentifier)
+    {
+        if (empty($imageSettings['width'])) {
+            throw new MissingArgumentsException('No arguments were given to placeholder controller');
+        }
+
+        $processor = $this->getProcessor();
+        $file = $processor->processFile($imageSettings, $targetFolderIdentifier);
+
+        if (!$file instanceof File) {
+            throw new FileDoesNotExistException('Could not process a file');
+        }
+
+        return $file;
+    }
+
+    /**
+     * @return AbstractProcessor
+     * @throws InvalidConfigurationException
+     */
+    protected function getProcessor() : AbstractProcessor
     {
         $configuration = ConfigurationUtility::getExtensionConfiguration();
 
@@ -107,8 +144,9 @@ class PlaceholderController
                 throw new InvalidConfigurationException('Invalid image source configuration!');
         }
 
+        /** @var AbstractProcessor $processor */
         $processor = GeneralUtility::makeInstance($processorClassName);
-        return $processor->processFile($imageSettings, $targetFolderIdentifier);
+        return $processor;
     }
 
     /**
